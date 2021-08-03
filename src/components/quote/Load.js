cb52d62718258d32f679f0a8cae1b9b9226b5eca
Tis Lais <tistististis@gmail.com>
2021-08-02 17:37:14 -0700

    Add header and get quote button

---
+++ src/components/quote/Load.js
@@ -0,0 +1,7 @@
+export default function Load() {
+  return (
+    <main className="flex pt-12 h-full w-full justify-center items-center">
+      <button className="col-span-2 bg-red-400 ml-4 rounded-full text-white w-36 p-2">Get Quote</button>
+    </main>
+  );
+}
---
+++ src/containers/TwinPeaksQuote.js
@@ -0,0 +1,7 @@
+export default function TwinPeaksQuote() {
+  return (
+    <main className="grid top-12 w-full bottom-0">
+      <h1>hello</h1>
+    </main>
+  );
+}
