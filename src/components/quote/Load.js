d5e96fa378493386ef838be7300aad8d3d544e59
Tis Lais <tistististis@gmail.com>
2021-08-02 17:41:38 -0700

    Add getQuote hook and display element

--- src/components/quote/Load.js
+++ src/components/quote/Load.js
@@ -1,7 +1,16 @@
-export default function Load() {
+import React, { useState } from 'react';
+
+const Load = () => {
+
+  const [quote, getQuote] = useState(0);
+
   return (
     <main className="flex pt-12 h-full w-full justify-center items-center">
-      <button className="col-span-2 bg-red-400 ml-4 rounded-full text-white w-36 p-2">Get Quote</button>
+      <button className="col-span-2 bg-red-400 ml-4 rounded-full text-white w-36 p-2"
+        onClick={getQuote}>Get Quote</button>
+      <p>{quote}</p>
     </main>
   );
 }
+
+export default Load;
