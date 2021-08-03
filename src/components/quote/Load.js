10227efc0f59170b2b2cbb152e08b76bbde53157
Tis Lais <tistististis@gmail.com>
2021-08-03 10:21:06 -0700

    Refactor to separate components

--- src/components/quote/Load.js
+++ src/components/quote/Load.js
@@ -1,26 +1,19 @@
-import React, { useState } from 'react';
-import { makeRequest } from '../../services/twinPeaksApi';
+import React from 'react';
+import Quote from './Quote';
 
-const Load = () => {
+const Load = ({ quote, getQuote }) => {
 
-  const [quote, setQuote] = useState();
-
-  const getQuote = async () => {
-    const resQuote = await makeRequest();
-    setQuote(resQuote);
-  }
 
   return (
     <main className="flex pt-12 h-full w-full flex-col justify-center items-center">
-      <button className="col-span-2 bg-blue-500 hover:bg-green-500 font-bold uppercase tracking-widest ml-4 rounded-full text-white w-36 p-2"
-        onClick={getQuote}>Load</button>
-      {quote ?
-        <>
-          <h1 className="font-bold mt-12 mb-6 text-3xl">{quote.name}:</h1>
-          <p className="w-1/4 text-lg">"{quote.quoteTextOnly}"</p> 
-        </>
-        : <p>click the button</p>
-        }
+      <button className="bg-pink-500 hover:bg-pink-400 font-bold uppercase tracking-widest rounded-full text-white px-8 py-4"
+        onClick={getQuote}>Load Quote</button>
+
+      {quote
+        ? <Quote quote={quote} />
+        : <p className="mt-12 text-lg font-semibold">Click the button</p>
+      }
+
     </main>
   );
 }
--- src/containers/TwinPeaksQuote.js
+++ src/containers/TwinPeaksQuote.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Load from '../components/quote/Load';
+import { makeRequest } from '../services/twinPeaksApi';
 
 export default function TwinPeaksQuote() {
+  
+  const [quote, setQuote] = useState();
+
+  const getQuote = async () => {
+    const resQuote = await makeRequest();
+    setQuote(resQuote);
+  }
+
   return (
     <div className="App h-screen">
       <Header />
-      <Load />
+      <Load quote={quote} getQuote={getQuote} />
     </div>
   );
 }
