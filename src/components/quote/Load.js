90511d20a08bb018517acb1f5c0f46ed203ff720
Tis Lais <tistististis@gmail.com>
2021-08-03 09:27:15 -0700

    Add functionality to load component

--- src/components/quote/Load.js
+++ src/components/quote/Load.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import { makeRequest } from '../../services/twinPeaksApi';
 
 const Load = () => {
 
-  const [quote, getQuote] = useState(0);
+  const [quote, setQuote] = useState();
+
+  const getQuote = async () => {
+    const resQuote = await makeRequest();
+    setQuote(resQuote);
+  }
 
   return (
-    <main className="flex pt-12 h-full w-full justify-center items-center">
-      <button className="col-span-2 bg-red-400 ml-4 rounded-full text-white w-36 p-2"
-        onClick={getQuote}>Get Quote</button>
-      <p>{quote}</p>
+    <main className="flex pt-12 h-full w-full flex-col justify-center items-center">
+      <button className="col-span-2 bg-blue-500 hover:bg-green-500 font-bold uppercase tracking-widest ml-4 rounded-full text-white w-36 p-2"
+        onClick={getQuote}>Load</button>
+      {quote ?
+        <>
+          <h1 className="font-bold mt-12 mb-6 text-3xl">{quote.name}:</h1>
+          <p className="w-1/4 text-lg">"{quote.quoteTextOnly}"</p> 
+        </>
+        : <p>click the button</p>
+        }
     </main>
   );
 }
